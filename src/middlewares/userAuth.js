916a58cf36886e4a31d736434dd8a16a9664e3ec
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-06-17 10:23:48 +0530

    updated authtoken user

--- src/middlewares/userAuth.js
+++ src/middlewares/userAuth.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config(); // Load environment variables
 const UserModel = require('../models/UserModel');
+const DealerModel = require('../models/DealerModel');
 
 function authenticateToken(req, res, next) {
   try {
@@ -90,8 +91,44 @@ function authenticateTokenDealer(req, res, next) {
   }
 }
 
+async function authorizeValidateDealer(req, res, next) {
+  try {
+    const token = req.headers['x-token-dealer'];
+    const dealerIdFromBody = req.params.dealer_id;
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    if (!dealerIdFromBody) {
+      return res.status(400).json({ error: 'Dealer ID is a required field' });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.decoded = decoded; // Save the decoded payload for further use
+
+    const dealer = await DealerModel.dealerById(dealerIdFromBody);
+
+    if (!dealer[0]) {
+      return res.status(401).json({ error: 'Unauthorized access' });
+    }
+
+    if (dealer[0].dealer_email !== req.decoded.email) {
+      return res.status(401).json({ error: 'Invalid dealer ID' });
+    }
+
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      return res.status(401).json({ error: 'Token expired' });
+    }
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
 module.exports = {
   authenticateToken,
   authorizeValidateUser,
-  authenticateTokenDealer
+  authenticateTokenDealer,
+  authorizeValidateDealer
 };
--- src/routes/dealer/dealer.js
+++ src/routes/dealer/dealer.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { login, getAll, getDealerById, addDealer, updateDealer, findDealer, changePassword, changeStatus, deleteDealer } = require('../../controllers/DealerController');
-const { authenticateTokenDealer } = require('../../middlewares/userAuth');
+const { authenticateTokenDealer, authorizeValidateDealer } = require('../../middlewares/userAuth');
 
 const { uploadDealer } = require('../../../config/fileUpload');
 
@@ -11,16 +11,16 @@ module.exports = (config) => {
     router.post('/login', login);
 
     router.get('/all', authenticateTokenDealer, getAll);
-    router.get('/:userid', authenticateTokenDealer, findDealer);
-    router.put('/status/:userid', authenticateTokenDealer, changeStatus);
-    router.put('/delete/:userid', authenticateTokenDealer, deleteDealer);
+    router.get('/:dealer_id', authenticateTokenDealer, findDealer);
+    router.put('/status/:dealer_id', authenticateTokenDealer, changeStatus);
+    router.put('/delete/:dealer_id', authenticateTokenDealer, deleteDealer);
 
-    // router.get('/me/:userid', authorizeValidateUser, getDealerById);
-    // router.put('/update/:userid', authorizeValidateUser, updateDealer);
+    router.get('/me/:userid', authorizeValidateDealer, getDealerById);
+    router.put('/update/:userid', authorizeValidateDealer, updateDealer);
 
-    // router.put('/changePassword/:userid', authorizeValidateUser, changePassword);
-    // router.put('/changeEmail/:userid', authorizeValidateUser, updateDealer);
-    // router.put('/deleteme/:userid', authorizeValidateUser, deleteDealer);
+    router.put('/changePassword/:userid', authorizeValidateDealer, changePassword);
+    router.put('/changeEmail/:userid', authorizeValidateDealer, updateDealer);
+    router.put('/deleteme/:userid', authorizeValidateDealer, deleteDealer);
 
     return router;
 };
