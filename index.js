816734dd52e7b52547ad4ea36218432b06aee271
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-06-25 14:26:12 +0530

    update token auth

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-    const { connection, config } = require('./config/connection');
-    const bodyParser = require('body-parser');
-    const express = require('express');
-    const app = express();
-    const routeHandler = require('./src/routes/index');
+const { connection, config } = require('./config/connection');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const routeHandler = require('./src/routes/index');
 
-    const { checkTables } = require('./config/table');
+const { checkTables } = require('./config/table');
 
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-    app.use(function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-        next();
-      });
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token, x-token-dealer'); // Include 'x-token' header
+    next();
+});;
 
-    app.use('/api', routeHandler(config));
+app.use('/api', routeHandler(config));
 
-    app.all('*', (req, res) => {
-        res.status(404).send({
-            error: 'resource not found',
-        });
+app.all('*', (req, res) => {
+    res.status(404).send({
+        error: 'resource not found',
     });
+});
 
-    const server = app.listen(config.port, () => {
-        console.log(`Server running at http://${config.hostname}:${server.address().port}/`);
-        checkTables();
-    });
+const server = app.listen(config.port, () => {
+    console.log(`Server running at http://${config.hostname}:${server.address().port}/`);
+    checkTables();
+});
