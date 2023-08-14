d8d96d7d8cd08ee6807158448a020e2063517a36
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-08-14 15:19:51 +0530

    updated

--- config/config.js
+++ config/config.js
@@ -6,11 +6,9 @@ module.exports = {
     port: process.env.PORT,
     connection: {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
-        connectionLimit: 10,
     },
     options: {
         encrypt: true // use encryption to secure the connection (recommended)
