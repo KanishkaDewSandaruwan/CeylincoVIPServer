df8eef097e222a065fc73b8cde314af45d80fc9f
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-08-15 20:35:33 +0530

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', ['http://ceylincocollection.dashboard.s3-website-us-east-1.amazonaws.com', 'http://localhost:3000']);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-token, x-token-dealer'); // Include 'x-token' header
     next();
