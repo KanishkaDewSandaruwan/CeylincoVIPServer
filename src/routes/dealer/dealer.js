226da647b70f55c5d191318ef300100b130d9779
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-08-27 23:34:26 +0530

    ss

--- src/routes/dealer/dealer.js
+++ src/routes/dealer/dealer.js
@@ -39,7 +39,7 @@ module.exports = (config) => {
     router.get('/verify/:token', validateDealer);
     
     router.get('/dash/all', authorizeAccessControll, getAll);
-    router.get('/dash:dealer_id', authorizeAccessControll, getDealerById);
+    router.get('/dash/:dealer_id', authorizeAccessControll, getDealerById);
     router.put('/dash/status/:dealer_id', authorizeAccessControll, changeStatus);
     router.put('/dash/delete/:dealer_id', authorizeAccessControll, deleteDealer);
     router.put('/dash/update/:dealer_id', authorizeAccessControll, updateDealer);
