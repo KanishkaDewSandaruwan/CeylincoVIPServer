9bf963db583c360e719dcd74b2d20b5051f7b73e
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-08-28 15:15:50 +0530

    dd

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -9,7 +9,7 @@ module.exports = (config) => {
     //no token authontication
     router.post('/login', login);
     router.post('/forget-password', fogetPassword);
-    router.post('/verify', restPassword);
+    router.get('/verify', restPassword);
 
     //user role permission authontication
     router.post('/create', authorizeAccessControll, addUser);
