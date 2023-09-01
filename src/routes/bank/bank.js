2fee9034fcb996f451671f9c1d6a56d8c9211c98
KanishkaDewSandaruwan <kanishkadewsandaruwan@gmail.com>
2023-09-01 23:54:12 +0530

    ss

--- src/routes/bank/bank.js
+++ src/routes/bank/bank.js
@@ -9,24 +9,24 @@ const {
     updatePaymentAccountField,
     getPaymentAccountById
  } = require('../../controllers/BankController');
-const { authorizeAccessControll, authenticateToken } = require('../../middlewares/userAuth');
+const { authorizeValidateDealer, authenticateToken } = require('../../middlewares/userAuth');
 
 module.exports = (config) => {
     const router = express.Router();
 
     // Create a new bank
-    router.post('/create', authorizeAccessControll, createPaymentAccount);
-    router.get('/all', authorizeAccessControll, getAllPaymentAccounts);
-    router.get('/:account_id', authorizeAccessControll, getPaymentAccountById);
-    router.get('/dealer/:dealerid', authorizeAccessControll, getPaymentAccountByDealerId);
-    router.put('/update/:account_id', authorizeAccessControll, updatePaymentAccount);
-    router.put('/update/single/:account_id/:field', authorizeAccessControll, updatePaymentAccountField);
-    router.delete('/delete/:account_id', authorizeAccessControll, updateIsDeleteFlag);
+    router.post('/create', authorizeValidateDealer, createPaymentAccount);
+    router.get('/all', authorizeValidateDealer, getAllPaymentAccounts);
+    router.get('/:account_id', authorizeValidateDealer, getPaymentAccountById);
+    router.get('/dealer/:dealerid', authorizeValidateDealer, getPaymentAccountByDealerId);
+    router.put('/update/:account_id', authorizeValidateDealer, updatePaymentAccount);
+    router.put('/update/single/:account_id/:field', authorizeValidateDealer, updatePaymentAccountField);
+    router.delete('/delete/:account_id', authorizeValidateDealer, updateIsDeleteFlag);
     
     router.get('/dash/all', authenticateToken, getAllPaymentAccounts);
     router.get('/dash/:account_id', authenticateToken, getPaymentAccountById);
     router.get('/dash/dealer/:dealerid', authenticateToken, getPaymentAccountByDealerId);
-    router.delete('/dash/delete/:account_id', authorizeAccessControll, updateIsDeleteFlag);
+    router.delete('/dash/delete/:account_id', authenticateToken, updateIsDeleteFlag);
 
     return router;
 };
